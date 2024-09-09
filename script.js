79d57684fe7b9322b854381530f1dd5b76fd1591
nataliaSiahaan <nataliatasyasiahaan@gmail.com>
2024-09-09 19:26:33 +0700

    menit 10

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 // "hello"   			{ h: 1, e: 1, l: 2, o: 1 }
 // "aabbbcc"			{ a: 2, b: 3, c: 2 }
 
-function hitungkarakter(){
-    if(hasil[karakter])
+function hitungkarakter(str){
+
+    // if(hasil[karakter])
+    for(let i=0; i < str.length; i++){
+        let karakter = str[i];
+
+        if(hasil[karakter])
+    }
 }
